dbcf1d6f074d999e7e73abacff00474d6289a5f4
tas_admin <tas_admin@hq.int.unesco.org>
2023-11-26 21:30:27 +0500

    init prj

---
+++ ComponentRepair/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComponentRepair.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
