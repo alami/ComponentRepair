f9cf0f957fa11ff40b396eaf86a550283923b7af
tas_admin <tas_admin@hq.int.unesco.org>
2023-12-01 15:28:37 +0500

    edit,delete of Component

--- ComponentRepair/Controllers/ComponentController.cs
+++ ComponentRepair/Controllers/ComponentController.cs
@@ -34,5 +34,87 @@ namespace ComponentRepair.Controllers
             }
             return View(obj);
         }
+        //GET Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Component.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //POST Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Component obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Component.Update (obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Component(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Component.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //POST Component
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ComponentPost(int? id)
+        {
+            var obj = _db.Component.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Component.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Component.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //POST Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Component.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Component.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
